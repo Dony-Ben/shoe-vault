8cd38bfded7cc32d5bdd24239f76011a35a3a6f0
Dony <donybenny612@gmail.com>
2025-08-08 17:06:30 +0530

    bug fixed

--- controllers/user/orderController.js
+++ controllers/user/orderController.js
@@ -342,7 +342,7 @@ const razorpaySuccessPage = async (req, res) => {
 
         // Use razorpay_order_id for signature verification (this is the key fix)
         console.log("Attempting to verify payment with the following details from controll:");
-        console.log("Order ID:", order_id);
+        console.log("Order ID:", orderId);
         console.log("Payment ID:", payment_id);
         console.log("Signature:", signature);
         const isPaymentVerified = verifyRazorpayPayment(
